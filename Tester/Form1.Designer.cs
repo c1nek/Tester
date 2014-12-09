2f9e15ff96efc8e70d8651885ea39bdcc009e2e2
c1nek <gumball300@gmail.com>
2014-12-09 01:50:24 +0100

    Dżony
    ty gamoniu

---
+++ Tester/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pIikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zakończToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.debug = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wczytaj ciąg";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pIikToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(478, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pIikToolStripMenuItem
+            // 
+            this.pIikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramieToolStripMenuItem,
+            this.zakończToolStripMenuItem});
+            this.pIikToolStripMenuItem.Name = "pIikToolStripMenuItem";
+            this.pIikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.pIikToolStripMenuItem.Text = "PIik";
+            // 
+            // oProgramieToolStripMenuItem
+            // 
+            this.oProgramieToolStripMenuItem.Name = "oProgramieToolStripMenuItem";
+            this.oProgramieToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.oProgramieToolStripMenuItem.Text = "O programie";
+            this.oProgramieToolStripMenuItem.Click += new System.EventHandler(this.oProgramieToolStripMenuItem_Click);
+            // 
+            // zakończToolStripMenuItem
+            // 
+            this.zakończToolStripMenuItem.Name = "zakończToolStripMenuItem";
+            this.zakończToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.zakończToolStripMenuItem.Text = "Zakończ";
+            this.zakończToolStripMenuItem.Click += new System.EventHandler(this.zakończToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Uruchom testy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // debug
+            // 
+            this.debug.Location = new System.Drawing.Point(12, 303);
+            this.debug.Multiline = true;
+            this.debug.Name = "debug";
+            this.debug.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.debug.Size = new System.Drawing.Size(454, 245);
+            this.debug.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(454, 68);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Testy statystyczne";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(11, 19);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(93, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Test par bitów";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(11, 41);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(143, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Test pojedyńczych bitów";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(418, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 20);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 111);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(442, 27);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(11, 88);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Test pokerowy";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.pictureBox3);
+            this.groupBox2.Controls.Add(this.progressBar3);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 130);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(454, 167);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Testy FIPS";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(313, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "20";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Długość serii";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(418, 13);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 17);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(6, 42);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(442, 28);
+            this.progressBar3.TabIndex = 4;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(11, 19);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(107, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Test długich serii";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 560);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.debug);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Tester Ciągów";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pIikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakończToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox debug;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Tester/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tester
+{
+    public partial class Form1 : Form
+    {
+        String ciag;
+        public Form1()
+        {
+            InitializeComponent();
+            debug.AppendText("[" + DateTime.Now + "] " + "Start programu.\n");
+            debug.AppendText("[" + DateTime.Now + "] " + "Miłego testowania :)\n");
+        }
+
+        private byte[] przepiszNaBity(String tekst)
+        {
+            debug.AppendText("[" + DateTime.Now + "] " + "Przepisanie stringa na tablice bitów...\n");
+            char[] x;
+            x = tekst.ToCharArray();
+            byte[] tab = new byte[x.Length];
+            for (int oooU = 0; oooU < x.Length; oooU++)
+                tab[oooU] = (byte)(x[oooU] - 48);
+
+            debug.AppendText("[" + DateTime.Now + "] " + "Przepisano!\n");
+
+            return tab;
+        }
+
+        private void testPokerowy(byte[] ciag, ProgressBar progress)
+        {
+            int[,] tab = new int[,] { 
+            { 0000, 0 }, 
+            { 0001, 0 }, 
+            { 0010, 0 }, 
+            { 0011, 0 },
+            { 0100, 0 }, 
+            { 0101, 0 },
+            { 0110, 0 },
+            { 0111, 0 }, 
+            { 1000, 0 }, 
+            { 1001, 0 }, 
+            { 1010, 0 },
+            { 1011, 0 }, 
+            { 1100, 0 },
+            { 1101, 0 },
+            { 1110, 0 },
+            { 1111, 0 },};
+
+            progress.Value = 0;
+            progress.Maximum = ciag.Length;
+            int temp = 0;
+            String xaxa;
+            double x=0, temp1=0, X=0;
+
+            for (int i = 0; i < ciag.Length; i=i+4)
+            {
+                xaxa = Convert.ToString(Convert.ToString(ciag[i]) + Convert.ToString(ciag[i + 1]) + Convert.ToString(ciag[i + 2]) + Convert.ToString(ciag[i + 3]));
+                temp = Convert.ToInt32(xaxa);
+                progress.Value += 4;
+                for (int j = 0; j < 16; j++)
+                {
+                    if (temp == tab[j, 0])
+                    {
+                        tab[j, 1]++;
+                        break;
+                    }
+
+                }
+   
+            }
+            for (int k = 0; k < 16; k++)
+            {
+                temp1 = (double)tab[k, 1] * (double)tab[k, 1];
+                x += temp1;
+            }
+            X = (double)16 / ((double)ciag.Length / 4) * x - 5000;
+            debug.AppendText("[" + DateTime.Now + "] " + "Wystąpienia\n");
+            debug.AppendText("___________________________________________\n");
+            for (int i = 0; i < 16; i++)
+            {
+                debug.AppendText(tab[i,0]+ "\t"+ tab[i,1] + "\n");
+            }
+            debug.AppendText("___________________________________________\n");
+
+            if (X > 2.16 && X<46.17)
+            {
+                pictureBox1.Image = Tester.Properties.Resources.tick;
+                debug.AppendText("[" + DateTime.Now + "] " + "Test zaliczony!\n");
+                debug.AppendText("[" + DateTime.Now + "] " + "X= " + X + "\n");
+            }
+            else
+            {
+                pictureBox1.Image = Tester.Properties.Resources.BlotterWarningRed;
+                debug.AppendText("[" + DateTime.Now + "] " + "Test niezaliczony!\n");
+                debug.AppendText("[" + DateTime.Now + "] " + "X nie mieści sie w zaresie! "+ X +"\n");
+            }
+
+            
+        }
+
+        private void testPojedynczychBitow(byte[] ciag, ProgressBar progress)
+        {
+            int x0 = 0, x1 = 0;
+            double s;
+            for (int i = 0; i < ciag.Length - 1; i++)
+            {
+                if (ciag[i] == 0)
+                {
+                    x0++;
+                }
+                else
+                {
+                    x1++;
+                }
+            }
+            if (ciag[ciag.Length - 1] == 0)
+            {
+                x0++;
+            }
+            else
+            {
+                x1++;
+            }
+
+
+            s = (double)(Math.Pow(x0 - x1, 2)) / (double)ciag.Length;
+
+            debug.AppendText("[" + DateTime.Now + "] " + "Rozkład wynosi: " + s + "\n");
+            label4.Text = "S= " + s;
+        }
+
+       
+        private void testDlugiejSerii(byte[] ciag, int dlugosc, ProgressBar progress)
+        {
+            debug.AppendText("[" + DateTime.Now + "] " + "Wskazana maksymalna długość serii: " + dlugosc +" \n");
+            int seriaMax = 1;
+            int seria = 1;
+            progress.Value = 0;
+            progress.Maximum = ciag.Length-2;
+            for (int i = 0; i < ciag.Length-1; i++)
+            {
+                if (ciag[i] == ciag[i + 1])
+                {
+                    seria++;
+                }
+                else 
+                    if (seria > seriaMax)
+                {
+                    seriaMax = seria;
+                    seria = 1;
+                }
+                progress.Value = i;
+                progress.Update();
+            }
+            if (seriaMax < dlugosc)
+            {
+                pictureBox3.Image = Tester.Properties.Resources.tick;
+                debug.AppendText("[" + DateTime.Now + "] " + "Test zaliczony!\n");
+                debug.AppendText("[" + DateTime.Now + "] " + "Maksymalna długość serii: " + seriaMax + "\n");
+            }
+            else
+            {
+                pictureBox3.Image = Tester.Properties.Resources.BlotterWarningRed;
+                debug.AppendText("[" + DateTime.Now + "] " + "Test niezaliczony!\n");
+                debug.AppendText("[" + DateTime.Now + "] " + "Maksymalna długość serii: " + seriaMax + "\n");
+            }
+
+        }
+
+        private void testParBitów(byte[] ciag)
+        {
+            int x0 = 0, x1 = 0, x00 = 0, x01 = 0, x10 = 0, x11 = 0;
+            double s;
+            int xx = 0;
+
+            for (int i = 0; i < ciag.Length-1; i++)
+            {
+                if (ciag[i] == 0)
+                {
+                    x0++;
+                }
+                else
+                {
+                    x1++;
+                }
+                if (ciag[i] == 0 && ciag[i + 1] == 0)
+                {
+                    x00++;
+                }
+                else if (ciag[i] == 0 && ciag[i + 1] == 1)
+                {
+                    x01++;
+                }
+                else if (ciag[i] == 1 && ciag[i + 1] == 0)
+                {
+                    x10++;
+                }
+                else if (ciag[i] == 1 && ciag[i + 1] == 1)
+                {
+                    x11++;
+                }
+                xx = i;
+            }
+            if (ciag[ciag.Length-1] == 0)
+            {
+                x0++;
+            }
+            else
+            {
+                x1++;
+            }
+
+            double a = 0, b = 0, c = 0, d = 0;
+            a = (double)4 / (double)(xx+1);
+            b = x00 * x00 + x01 * x01 + x10 * x10 + x11 * x11;
+            c = (double)2 / (double)(xx + 2);
+            d = (x0 * x0 + x1 * x1);
+            s = ((a * b) - (c * d)) + 1;
+
+
+            debug.AppendText("[" + DateTime.Now + "] " + "Rozkład wynosi: " + s + "\n");
+            label3.Text = "S= " + s;
+
+            
+        }
+        private void oProgramieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tester ciągów \nMarcin Gluza \n2014 \n\ngumball300@gmail.com", "Autor");
+        }
+
+        private void zakończToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    debug.AppendText("[" + DateTime.Now + "] " + "Wczytano dane z pliku: " + openFileDialog1.FileName +"\n");
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            ciag = File.ReadAllText(openFileDialog1.FileName);
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd. Nie można odczytać wskazenego pliku! \n " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            debug.AppendText("[" + DateTime.Now + "] " + "Uruchamiam testy! \n");
+            byte[] kod = przepiszNaBity(ciag);
+            debug.AppendText("[" + DateTime.Now + "] " + "Długość ciągu to: " + kod.Length + "\n");
+  
+            if (checkBox1.Checked == true)
+            {
+                debug.AppendText("[" + DateTime.Now + "] " + "Test pokerowy.\n");
+                testPokerowy(kod, progressBar1);
+            }
+            if (checkBox2.Checked == true)
+            {
+                debug.AppendText("[" + DateTime.Now + "] " + "Test pojedyńczych bitów.\n");
+                testPojedynczychBitow(kod, progressBar1);
+            }
+            if (checkBox3.Checked == true)
+            {
+                debug.AppendText("[" + DateTime.Now + "] " + "Test długiej serii\n");
+                testDlugiejSerii(kod, Convert.ToInt16(textBox2.Text), progressBar3);
+            }
+            if (checkBox4.Checked == true)
+            {
+                debug.AppendText("[" + DateTime.Now + "] " + "Test par bitów\n");
+                testParBitów(kod);
+
+            }
+
+                
+        }
+    }
+}
